fd3af48df717c8f0e50e30f52bcb0a8791f7a7ed
Matthew <philosopheratlarge@protonmail.com>
2020-12-27 17:17:52 -0500

    Expanded before_find function to include recaptcha check

--- event_listeners.py
+++ event_listeners.py
@@ -4,56 +4,35 @@ from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEven
 from selenium.webdriver.common.keys import Keys
 import random
 import time
-import datimetime
+import datetime
 
 class BotListener(AbstractEventListener):
-    def before_navigate_to(self, url, driver):
-        print("Before navigate to %s" % url)
-
-    def after_navigate_to(self, url, driver):
-        print("After navigate to %s" % url)
-
     def before_find(self, by, value, driver):
-        # Random sleep (float) to prevent bot detection!
+        # Random sleep to prevent bot detection!
         print("Sleeping to avoid bot detection...")
-        time.sleep(random.uniform(1.0, 2.0))
-        # Check for recaptcha
-        recaptcha_tries = 10
+        #time.sleep(random.uniform(1.0, 2.0))
+        time.sleep(random.randint(1, 3))
         recaptcha_sleep = 10
-        for attempt in range(recaptcha_tries):
+        find_tries = 10
+        for find_try in range(find_tries):
             try:
-                # recaptcha element
-                driver.find_element_by_xpath(
-                    "//*[contains(@class, 're-captcha')]")
-                # Wait 10 seconds for user to process captcha
-                print("Recaptcha found on attempt #%s" % attempt)
-                print("Waiting 10 seconds for recaptcha")
-                time.sleep(recaptcha_sleep)
-            except:
-                print("No recaptcha found!")
-                # break from attempt loop
+                driver.find_element(by, value)
                 break
-
-    def before_click(self, element, driver):
-        pass
-
-    def after_click(self, element, driver):
-        pass
-
-    def after_navigate_forward(self, driver):
-        print("after_navigate_forward")
-
-    def before_navigate_forward(self, driver):
-        print("before_navigate_forward")
-
-    def after_navigate_back(self, driver):
-        print("after_navigate_back")
-
-    def before_navigate_back(self, driver):
-        print("before_navigate_back")
-
-    def before_change_value_of(self, element, driver):
-        print("before_change_value_of")
+            except:
+                print("Couldn't find element, checking for recaptcha")
+                while True:
+                    try:
+                        # recaptcha element
+                        driver.find_element_by_xpath(
+                            "//*[contains(@class, 're-captcha')]")
+                        # Wait 10 seconds for user to process captcha
+                        print("Recaptcha found!")
+                        print("Waiting for human intervention!")
+                        time.sleep(recaptcha_sleep)
+                    except:
+                        print("No recaptcha found!")
+                        # break from attempt loop
+                        break
 
     def on_exception(self, exception, driver):
         screenshot_name = "%s-exception.png" % datetime.now()
